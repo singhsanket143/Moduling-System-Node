bc793a0d4a8a44bceb067b1dc4f37a560a2f34d4
Sanket Singh <singhsanket143@gmail.com>
2024-10-14 22:28:22 +0530

    Commonjs

---
+++ globals.js
@@ -0,0 +1,15 @@
+// __dirname
+
+console.log(__dirname); // It will print the current directory path
+
+// module
+
+console.log(module); // It will print the module object
+
+console.log(__filename); // It will print the current file name
+
+console.log(process); // It will print the process object
+
+console.log(require); // It will print the require function
+
+console.log(global);
---
+++ main.js
@@ -0,0 +1,4 @@
+const Searching = require('./searching.js');
+
+console.log(Searching);
+console.log(Searching.linearSearch([10, 15, 20, 25, 30], 15)); // 1
